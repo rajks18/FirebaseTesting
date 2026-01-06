28ee8e0baa3eaeddfa1c5e067da1c39739672c26
Rajkumar Sarker <sardonic100@gmail.com>
2026-01-06 19:14:31 +0600

    Update Logout.jsx

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -18,7 +18,7 @@ const Logout = () => {
   };
 
   return (
-    <div>
+    <div className="my-8">
       <button onClick={handleLogout} className="px-5 py-2 bg-red-600 text-white font-semibold rounded">Logout</button>
     </div>
   );
